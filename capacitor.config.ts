99233ce61a5ab0ac792502350ed59bfcf97490de
seupaulao <seupaulao@gmail.com>
2026-06-18 20:45:57 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pjla_resumo_financeiro',
+  webDir: 'dist'
+};
+
+export default config;
